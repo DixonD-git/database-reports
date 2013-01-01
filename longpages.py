e30d7fde2b29d1b7f20731ce9040fa3439728b8d
dixond <dixond@acm.lviv.ua>
2013-01-02 00:33:25 +0200

    Translated texts to Ukrainian

--- longpages.py
+++ longpages.py
@@ -23,38 +23,41 @@ import oursql
 import settings
 import wikipedia as pywikibot
 import login
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
 
 report_title = settings.rootpage + u'Довгі сторінки'
 
 report_template = u'''
-Long pages; data as of <onlyinclude>%s</onlyinclude>.
+Довгі сторінки; дані станом на <onlyinclude>{0}</onlyinclude>.
 
-== Specified talk pages ==
-Talk pages whose page length is greater than 140,000 bytes \
-(excluding subpages and pages in the user space).
+== Визначені сторінки обговорення ==
+Сторінки обговорення, довжина яких перевищує 140 000 байтів \
+(не включаючи підсторінки і сторінки у користувацькому просторі).
 
-{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
 ! No.
 ! Page
 ! Length
 ! Last edit
 |-
-%s
-|}
+{1}
+|}}
 
-== All pages ==
-All pages whose page length is greater than 500,000 bytes.
+== Всі сторінки ==
+Всі сторінки, довжина яких перевищує 500 000 байтів.
 
-{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
 ! No.
 ! Page
 ! Length
 ! Last edit
 |-
-%s
-|}
+{2}
+|}}
 '''
 
 site = pywikibot.Site(code = settings.sitecode)
@@ -77,7 +80,7 @@ SELECT
 FROM page
 JOIN toolserver.namespace
 ON page_namespace = ns_id
-AND dbname = "%s"
+AND dbname = "{}"
 JOIN revision
 ON rev_page = page_id
 WHERE page_len > 140000
@@ -89,24 +92,24 @@ AND rev_timestamp = (SELECT
                      FROM revision
                      WHERE rev_page = page_id)
 ORDER BY page_len, page_namespace ASC;
-''' % settings.dbname)
+'''.format(settings.dbname))
 
 i = 1
 output1 = []
 for row in cursor.fetchall():
-    ns_name = u'%s' % unicode(row[0], 'utf-8')
-    page_title = u'%s' % unicode(row[1], 'utf-8')
+    ns_name = unicode(row[0], 'utf-8')
+    page_title = unicode(row[1], 'utf-8')
     if ns_name:
-        page_title = '{{dbr link|1=%s:%s}}' % (ns_name, page_title)
+        page_title = u'{{{{dbr link|1={0}:{1}}}}}'.format(ns_name, page_title)
     else:
-        page_title = '{{dbr link|1=%s}}' % (page_title)
+        page_title = u'{{{{dbr link|1={0}}}}}'.format(page_title)
     page_len = row[2]
     rev_timestamp = row[3]
-    table_row = u'''| %d
-| %s
-| %s
-| %s
-|-''' % (i, page_title, page_len, rev_timestamp)
+    table_row = u'''| {}
+| {}
+| {}
+| {}
+|-'''.format(i, page_title, page_len, rev_timestamp)
     output1.append(table_row)
     i += 1
 
@@ -120,7 +123,7 @@ SELECT
 FROM page
 JOIN toolserver.namespace
 ON page_namespace = ns_id
-AND dbname = "%s"
+AND dbname = "{}"
 JOIN revision
 ON rev_page = page_id
 WHERE page_len > 500000
@@ -129,33 +132,34 @@ AND rev_timestamp = (SELECT
                      FROM revision
                      WHERE rev_page = page_id)
 ORDER BY page_len, page_namespace ASC;
-''' % settings.dbname)
+'''.format(settings.dbname))
 
 i = 1
 output2 = []
 for row in cursor.fetchall():
-    ns_name = u'%s' % unicode(row[0], 'utf-8')
-    page_title = u'%s' % unicode(row[1], 'utf-8')
+    ns_name = unicode(row[0], 'utf-8')
+    page_title = unicode(row[1], 'utf-8')
     if ns_name:
-        page_title = '{{dbr link|1=%s:%s}}' % (ns_name, page_title)
+        page_title = u'{{{{dbr link|1={0}:{1}}}}}'.format(ns_name, page_title)
     else:
-        page_title = '{{dbr link|1=%s}}' % (page_title)
+        page_title = u'{{{{dbr link|1={0}}}}}'.format(page_title)
     page_len = row[2]
     rev_timestamp = row[3]
-    table_row = u'''| %d
-| %s
-| %s
-| %s
-|-''' % (i, page_title, page_len, rev_timestamp)
+    table_row = u'''| {}
+| {}
+| {}
+| {}
+|-'''.format(i, page_title, page_len, rev_timestamp)
     output2.append(table_row)
     i += 1
 
 cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
 rep_lag = cursor.fetchone()[0]
+
 current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
 
 report = pywikibot.Page(site = site, title = report_title)
-report_text = report_template % (current_of, '\n'.join(output1), '\n'.join(output2))
+report_text = report_template.format(unicode(current_of, 'utf-8'), u'\n'.join(output1), u'\n'.join(output2))
 report.put(report_text, comment = settings.editsumm)
 
 cursor.close()
