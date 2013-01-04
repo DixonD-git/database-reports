ca787da9b68e2a0f96e00d5a3247cdf8c13f7a88
dixond <dixond@acm.lviv.ua>
2013-01-04 19:47:03 +0200

    Rewrote the "Pages containing an unusually high number of non-free files" report

--- pageslotnonfree.py
+++ pageslotnonfree.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python2.5
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python2.7
 
-# Copyright 2008 bjweeks, MZMcBride
+# Copyright 2008-2013 bjweeks, MZMcBride, DixonD-git
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,33 +16,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import datetime
-import MySQLdb
-import wikitools
+import common
 import settings
 
-report_title = settings.rootpage + 'Pages containing an unusually high number of non-free files'
+report_name = u'Сторінки, що включають значну кількість невільних файлів'
+
+non_free_media_category = u'Невільні_файли'
 
 report_template = u'''
-Pages containing an unusually high number of non-free files; \
-data as of <onlyinclude>%s</onlyinclude>.
+Сторінки, що включаються більше, ніж 6 невільних файлів; дані станом на <onlyinclude>{0}</onlyinclude>.
 
-{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
-! No.
-! Page
-! Non-free files
+! №
+! Сторінка
+! Невільні файли
 |-
-%s
-|}
+{1}
+|}}
 '''
 
-wiki = wikitools.Wiki(settings.apiurl)
-wiki.login(settings.username, settings.password)
-
-conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
-cursor = conn.cursor()
-cursor.execute('''
+sqlQuery = '''
 /* pageslotnonfree.py SLOW_OK */
 SELECT
   imgtmp.page_namespace,
@@ -58,46 +53,40 @@ JOIN (SELECT
         il_to
       FROM page AS pg2
       JOIN toolserver.namespace
-      ON dbname = %s
+      ON dbname = "{0}"
       AND pg2.page_namespace = ns_id
       JOIN imagelinks
       ON il_from = page_id) AS imgtmp
 ON il_to = pg1.page_title
 WHERE pg1.page_namespace = 6
-AND cl_to = 'All_non-free_media'
+AND cl_to = '{1}'
 GROUP BY imgtmp.page_namespace, imgtmp.page_title
 HAVING COUNT(cl_to) > 6
 ORDER BY COUNT(cl_to) DESC;
-''' , settings.dbname)
+'''.format(settings.dbname, non_free_media_category.encode('utf-8'))
+
+site, db, cursor = common.prepareEnvironment()
+
+cursor.execute(sqlQuery)
 
 i = 1
 output = []
 for row in cursor.fetchall():
     page_namespace = row[0]
-    ns_name = u'%s' % unicode(row[1], 'utf-8')
-    page_title = u'%s' % unicode(row[2], 'utf-8')
+    ns_name = unicode(row[1], 'utf-8')
+    page_title = unicode(row[2], 'utf-8')
     count = row[3]
     if page_namespace == 6 or page_namespace == 14:
-        page_title = u'[[:%s:%s]]' % (ns_name, page_title)
+        page_title = u'[[:{0}:{1}]]'.format(ns_name, page_title)
     elif page_namespace == 0:
-        page_title = u'[[%s]]' % page_title
+        page_title = u'[[{0]]'.format(page_title)
     else:
-        page_title = u'[[%s:%s]]' % (ns_name, page_title)
-    table_row = u'''| %d
-| %s
-| %s
-|-''' % (i, page_title, count)
+        page_title = u'[[{0}:{1}]]'.format(ns_name, page_title)
+    table_row = u'''| {0}
+| {1}
+| {2}
+|-'''.format(i, page_title, count)
     output.append(table_row)
     i += 1
 
-cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
-rep_lag = cursor.fetchone()[0]
-current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
-
-report = wikitools.Page(wiki, report_title)
-report_text = report_template % (current_of, '\n'.join(output))
-report_text = report_text.encode('utf-8')
-report.edit(report_text, summary=settings.editsumm, bot=1)
-
-cursor.close()
-conn.close()
+common.finishReport(db, cursor, site, report_name, report_template, [output], __file__)
