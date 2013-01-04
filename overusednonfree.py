3152a63e8561ab48fe4fbe4abf23597b3911c74d
dixond <dixond@acm.lviv.ua>
2013-01-04 18:01:05 +0200

    Rewrote the "Overused non-free files" report

--- overusednonfree.py
+++ overusednonfree.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python2.5
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python2.7
 
-# Copyright 2008 bjweeks, MZMcBride
+# Copyright 2008-2013 bjweeks, MZMcBride, DixonD-git
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,32 +16,26 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import datetime
-import MySQLdb
-import wikitools
+import common
 import settings
 
-report_title = settings.rootpage + 'Overused non-free files'
+report_name = u'Невільні файли з надмірним використанням'
+non_free_media_category = u'Невільні_файли'
 
 report_template = u'''
-Non-free files used on more than four pages; data as of <onlyinclude>%s</onlyinclude>.
+Невільні файли, що використовуються на більше, ніж 4 сторінках; дані станом на <onlyinclude>{0}</onlyinclude>.
 
-{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
+{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
 |- style="white-space:nowrap;"
-! No.
-! File
-! Uses
+! №
+! Файл
+! Використання
 |-
-%s
-|}
+{1}
+|}}
 '''
 
-wiki = wikitools.Wiki(settings.apiurl)
-wiki.login(settings.username, settings.password)
-
-conn = MySQLdb.connect(host=settings.host, db=settings.dbname, read_default_file='~/.my.cnf')
-cursor = conn.cursor()
-cursor.execute('''
+sqlQuery = '''
 /* overusednonfree.py SLOW_OK */
 SELECT
   ns_name,
@@ -53,39 +48,33 @@ JOIN (SELECT
         page_title
       FROM page
       JOIN toolserver.namespace
-      ON dbname = %s
+      ON dbname = "{0}"
       AND page_namespace = ns_id
       JOIN categorylinks
       ON cl_from = page_id
-      WHERE cl_to = 'All_non-free_media'
+      WHERE cl_to = "{1}"
       AND page_namespace = 6) AS pgtmp
 ON pgtmp.page_title = il_to
 GROUP BY il_to
 HAVING COUNT(*) > 4
 ORDER BY COUNT(*) ASC;
-''' , settings.dbname)
+'''.format(settings.dbname, non_free_media_category.encode('utf-8'))
+
+site, db, cursor = common.prepareEnvironment()
+
+cursor.execute(sqlQuery)
 
 i = 1
 output = []
 for row in cursor.fetchall():
-    ns_name = u'%s' % unicode(row[0], 'utf-8')
-    page_title = u'[[:%s:%s|%s]]' % (ns_name, unicode(row[1], 'utf-8'), unicode(row[1], 'utf-8'))
+    ns_name = unicode(row[0], 'utf-8')
+    page_title = u'[[:{0}:{1}|{2}]]'.format(ns_name, unicode(row[1], 'utf-8'), unicode(row[1], 'utf-8'))
     count = row[2]
-    table_row = u'''| %d
-| %s
-| %s
-|-''' % (i, page_title, count)
+    table_row = u'''| {0}
+| {1}
+| {2}
+|-'''.format(i, page_title, count)
     output.append(table_row)
     i += 1
 
-cursor.execute('SELECT UNIX_TIMESTAMP() - UNIX_TIMESTAMP(rc_timestamp) FROM recentchanges ORDER BY rc_timestamp DESC LIMIT 1;')
-rep_lag = cursor.fetchone()[0]
-current_of = (datetime.datetime.utcnow() - datetime.timedelta(seconds=rep_lag)).strftime('%H:%M, %d %B %Y (UTC)')
-
-report = wikitools.Page(wiki, report_title)
-report_text = report_template % (current_of, '\n'.join(output))
-report_text = report_text.encode('utf-8')
-report.edit(report_text, summary=settings.editsumm, bot=1)
-
-cursor.close()
-conn.close()
+common.finishReport(db, cursor, site, report_name, report_template, [output], __file__)
