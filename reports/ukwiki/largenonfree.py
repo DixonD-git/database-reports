f98f9fca1841141b791d3a58cd017a9aaf8f6d58
dixond <dixond@acm.lviv.ua>
2014-02-02 21:07:43 +0200

    Converted largenonfree report

--- reports/ukwiki/largenonfree.py
+++ reports/ukwiki/largenonfree.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python2.7
 
-# Copyright 2009-2013 bjweeks, MZMcBride, DixonD-git
+# Copyright 2009-2014 bjweeks, MZMcBride, DixonD-git
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,61 +16,39 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import common
-import settings
+import reports
 
-report_name = u'Великі невільні файли'
-non_free_media_category = u'Невільні_файли'
 
-report_template = u'''
-Невільні файли, розмір яких більший, ніж 999999 байтів; дані станом на <onlyinclude>{0}</onlyinclude>.
+class report(reports.report):
+    def get_title(self):
+        return u'Великі невільні файли'
 
-{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
-|- style="white-space:nowrap;"
-! №
-! Файл
-! Розмір
-|-
-{1}
-|}}
-'''
+    def get_preamble_template(self):
+        return u'Невільні файли, розмір яких більший, ніж 999999 байтів; дані станом на <onlyinclude>%s</onlyinclude>.'
 
-sqlQuery = '''
-/* largenonfree.py SLOW_OK */
-SELECT
-  ns_name,
-  page_title,
-  img_size
-FROM page
-JOIN toolserver.namespace
-ON dbname = "{0}"
-AND page_namespace = ns_id
-JOIN image
-ON img_name = page_title
-JOIN categorylinks
-ON cl_from = page_id
-WHERE page_namespace = 6
-AND cl_to = "{1}"
-AND img_size > 999999
-ORDER BY img_size DESC;
-'''.format(settings.dbname, non_free_media_category.encode('utf-8'))
+    def get_table_columns(self):
+        return [u'Файл', u'Розмір']
 
-site, db, cursor = common.prepareEnvironment()
+    def get_table_rows(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('''
+        /* largenonfree.py SLOW_OK */
+        SELECT
+          CONVERT(page_title USING utf8),
+          img_size
+        FROM page
+        JOIN image
+        ON img_name = page_title
+        JOIN categorylinks
+        ON cl_from = page_id
+        WHERE page_namespace = 6
+        AND cl_to = "%s"
+        AND img_size > 999999
+        ORDER BY img_size DESC;
+        ''' % u'Невільні_файли'.encode('utf-8'))
 
-cursor.execute(sqlQuery)
+        for page_title, img_size in cursor:
+            page_title = u'[[:Файл:%s|%s]]' % (page_title, page_title)
+            yield [page_title, img_size]
 
-i = 1
-output = []
-for row in cursor.fetchall():
-    ns_name = unicode(row[0], 'utf-8')
-    page_title = unicode(row[1], 'utf-8')
-    full_page_title = u'[[:{0}:{1}|{2}]]'.format(ns_name, page_title, page_title)
-    img_size = row[2]
-    table_row = u'''| {0}
-| {1}
-| {2}
-|-'''.format(i, full_page_title, img_size)
-    output.append(table_row)
-    i += 1
-
-common.finishReport(db, cursor, site, report_name, report_template, [output], __file__)
+        cursor.close()
