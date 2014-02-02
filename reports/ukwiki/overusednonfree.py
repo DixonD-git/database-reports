0710ca21653270c5f38976e34ee2b63cad00f2cd
dixond <dixond@acm.lviv.ua>
2014-02-02 21:18:50 +0200

    Converted overusednonfree report

--- reports/ukwiki/overusednonfree.py
+++ reports/ukwiki/overusednonfree.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python2.7
 
-# Copyright 2008-2013 bjweeks, MZMcBride, DixonD-git
+# Copyright 2008-2014 bjweeks, MZMcBride, DixonD-git
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,65 +16,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import common
-import settings
+import reports
 
-report_name = u'Невільні файли з надмірним використанням'
-non_free_media_category = u'Невільні_файли'
 
-report_template = u'''
-Невільні файли, що використовуються на більше, ніж 4 сторінках; дані станом на <onlyinclude>{0}</onlyinclude>.
+class report(reports.report):
+    def get_title(self):
+        return u'Невільні файли з надмірним використанням'
 
-{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
-|- style="white-space:nowrap;"
-! №
-! Файл
-! Використання
-|-
-{1}
-|}}
-'''
+    def get_preamble_template(self):
+        return u'Невільні файли, що використовуються на більше, ніж 4 сторінках; дані станом на <onlyinclude>%s</onlyinclude>.'
 
-sqlQuery = '''
-/* overusednonfree.py SLOW_OK */
-SELECT
-  ns_name,
-  page_title,
-  COUNT(*)
-FROM imagelinks
-JOIN (SELECT
-        page_id,
-        ns_name,
-        page_title
-      FROM page
-      JOIN toolserver.namespace
-      ON dbname = "{0}"
-      AND page_namespace = ns_id
-      JOIN categorylinks
-      ON cl_from = page_id
-      WHERE cl_to = "{1}"
-      AND page_namespace = 6) AS pgtmp
-ON pgtmp.page_title = il_to
-GROUP BY il_to
-HAVING COUNT(*) > 4
-ORDER BY COUNT(*) ASC;
-'''.format(settings.dbname, non_free_media_category.encode('utf-8'))
+    def get_table_columns(self):
+        return [u'Файл', u'Використання']
 
-site, db, cursor = common.prepareEnvironment()
+    def get_table_rows(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('''
+        /* overusednonfree.py SLOW_OK */
+        SELECT
+          CONVERT(page_title USING utf8),
+          COUNT(*)
+        FROM imagelinks
+        JOIN (SELECT
+                page_title
+              FROM page
+              JOIN categorylinks
+              ON cl_from = page_id
+              WHERE cl_to = "%s"
+              AND page_namespace = 6) AS pgtmp
+        ON pgtmp.page_title = il_to
+        GROUP BY il_to
+        HAVING COUNT(*) > 4
+        ORDER BY COUNT(*) DESC;
+        ''' % u'Невільні_файли'.encode('utf-8'))
 
-cursor.execute(sqlQuery)
+        for page_title, count in cursor:
+            page_title = u'[[:Файл:%s|%s]]' % (page_title, page_title)
+            yield [page_title, count]
 
-i = 1
-output = []
-for row in cursor.fetchall():
-    ns_name = unicode(row[0], 'utf-8')
-    page_title = u'[[:{0}:{1}|{2}]]'.format(ns_name, unicode(row[1], 'utf-8'), unicode(row[1], 'utf-8'))
-    count = row[2]
-    table_row = u'''| {0}
-| {1}
-| {2}
-|-'''.format(i, page_title, count)
-    output.append(table_row)
-    i += 1
-
-common.finishReport(db, cursor, site, report_name, report_template, [output], __file__)
+        cursor.close()
