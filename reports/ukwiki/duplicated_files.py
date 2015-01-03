d9229f640481e0c501aa3d02635432f6217b004b
DixonD-git <dixond@acm.lviv.ua>
2015-01-03 21:55:14 +0200

    Small fix

--- reports/ukwiki/duplicated_files.py
+++ reports/ukwiki/duplicated_files.py
@@ -36,7 +36,7 @@ class report(reports.report):
         SELECT u.img_name
         FROM ukwiki_p.image AS u
         INNER JOIN commonswiki_p.image AS c
-        ON c.img_name=en.img_name
+        ON c.img_name=u.img_name
         ORDER BY u.img_name;
         ''')
 
