e9197cccf5d9c4cc6fef39c85abc7f96e0c160ab
DixonD-git <dixond@acm.lviv.ua>
2015-01-04 02:26:14 +0200

    Converted filelessfilepages report

--- reports/ukwiki/filelessfilepages.py
+++ reports/ukwiki/filelessfilepages.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python2.7
 
-# Copyright 2009-2013 bjweeks, MZMcBride, DixonD-git
+# Copyright 2009-2015 bjweeks, MZMcBride, DixonD-git
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,62 +16,43 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import settings
-import common
+import reports
 
-report_name = u'Сторінки описів файлів без відповідного файлу'
 
-report_template = u'''
-Сторінки описів файлів без відповідного файлу; дані станом на <onlyinclude>{0}</onlyinclude>.
+class report(reports.report):
+    def get_title(self):
+        return u'Сторінки описів файлів без відповідного файлу'
 
-{{| class="wikitable sortable plainlinks" style="width:100%%; margin:auto;"
-|- style="white-space:nowrap;"
-! №
-! Сторінка
-|-
-{1}
-|}}
-'''
+    def get_preamble_template(self):
+        return u'Сторінки описів файлів без відповідного файлу; дані станом на <onlyinclude>%s</onlyinclude>.'
 
-sqlQuery = '''
-/* filelessfilepages.py SLOW_OK */
-SELECT
-  ns_name,
-  pg1.page_title
-FROM page AS pg1
-JOIN toolserver.namespace
-ON dbname = "{0}"
-AND pg1.page_namespace = ns_id
-WHERE NOT EXISTS (SELECT
-                    img_name
-                  FROM image
-                  WHERE img_name = pg1.page_title)
-AND NOT EXISTS (SELECT
-                  img_name
-                FROM commonswiki_p.image
-                WHERE img_name = CAST(pg1.page_title AS CHAR))
-AND NOT EXISTS (SELECT
-                  1
-                FROM commonswiki_p.page AS pg2
-                WHERE pg2.page_namespace = 6
-                AND pg2.page_title = CAST(pg1.page_title AS CHAR)
-                AND pg2.page_is_redirect = 1)
-AND pg1.page_namespace = 6
-AND pg1.page_is_redirect = 0;
-'''.format(settings.dbname)
+    def get_table_columns(self):
+        return [u'Файл']
 
-site, db, cursor = common.prepareEnvironment()
+    def get_table_rows(self, conn):
+        cursor = conn.cursor()
+        cursor.execute('''
+        /* filelessfilepages.py SLOW_OK */
+        SELECT
+          pg1.page_title
+        FROM page AS pg1
+        WHERE pg1.page_title NOT IN (
+            SELECT
+                img_name
+            FROM image
+            WHERE img_name = pg1.page_title)
+        AND pg1.page_title NOT IN (
+            SELECT
+                img_name
+            FROM commonswiki_p.image
+            WHERE img_name = pg1.page_title)
+        AND pg1.page_namespace = 6
+        AND pg1.page_is_redirect = 0
+        ORDER BY pg1.page_title;
+        ''')
 
-cursor.execute(sqlQuery)
+        for page_title, in cursor:
+            page_title = u'[[:Файл:%s|%s]]' % (page_title, page_title)
+            yield [page_title]
 
-i = 1
-output = []
-for row in cursor.fetchall():
-    page_title = u'[[:{0}:{1}|]]'.format(unicode(row[0], 'utf-8'), unicode(row[1], 'utf-8'))
-    table_row = u'''| {0}
-| {1}
-|-'''.format(i, page_title)
-    output.append(table_row)
-    i += 1
-
-common.finishReport(db, cursor, site, report_name, report_template, [output], __file__)
+        cursor.close()
